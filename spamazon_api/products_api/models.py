835430c7f0a9dc65d608e1449ca341db9df4a425
Andy checo <savedcity@Andys-MacBook-Air.local>
2021-05-28 14:54:57 -0500

    test

--- spamazon_api/products_api/models.py
+++ spamazon_api/products_api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=64)
-    price = models.CharField(max_length=9)
+    price = models.IntegerField()
     description = models.TextField()
     image = models.CharField(max_length=256)
     stock = models.IntegerField()
