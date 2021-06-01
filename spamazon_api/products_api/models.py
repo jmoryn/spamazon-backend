23dba64cdf30e706f66405ca72f6c51c6ead44cc
Andy checo <savedcity@Andys-MacBook-Air.local>
2021-06-01 14:25:44 -0500

    removed stock

--- spamazon_api/products_api/models.py
+++ spamazon_api/products_api/models.py
@@ -6,6 +6,5 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     image = models.CharField(max_length=256)
-    stock = models.IntegerField()
     created_at = models.DateField(auto_now_add=True)
     created_by = models.CharField(max_length=32)
